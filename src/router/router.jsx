f15d873500a8463be2041de7fe12bca2635a76a1
deepanshu <deepanshudashore@gmail.com>
2024-11-19 18:40:07 +0530

    contact form done for my side

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import App from '../App';
+import Contact from '../pages/contact us/Contact';
+import { Contact2 } from 'lucide-react';
+import ContactPage from '../pages/contact us/Contact2';
 
 const router =createBrowserRouter([
     {
@@ -15,7 +18,15 @@ const router =createBrowserRouter([
             {
              path:"/about",
              element:<h1 className='h-screen flex items-center justify-center'>this is about page</h1>
-            }
+            },
+            {
+                path:"/contact",
+                element:<Contact/>
+            },
+            {
+                path:"/contact2",
+                element:<ContactPage/>
+            },
         ]
     }
 ])
