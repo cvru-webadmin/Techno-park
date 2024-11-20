ee37b576f3eef6a52cdf36c41ecb05d9aa1667f0
deepanshu <deepanshudashore@gmail.com>
2024-11-20 15:41:00 +0530

    exmaple update

--- src/context/ExampleContext.jsx
+++ src/context/ExampleContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const ExampleContext= createContext();
 export const ExampleProvider=({children})=>{
-const{about,setAbout}=useState(null)
+const [about,setAbout]=useState(null)
     return(
         <ExampleContext.Provider value={{about,setAbout}}>
             {children}
