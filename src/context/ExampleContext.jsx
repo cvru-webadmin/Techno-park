7b8cf074c5c3396522968c430f82da8250ec05c6
deepanshu <deepanshudashore@gmail.com>
2024-11-20 15:24:42 +0530

    example null problem clear

--- src/context/ExampleContext.jsx
+++ src/context/ExampleContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const ExampleContext= createContext();
 export const ExampleProvider=({children})=>{
-const{about,setAbout}=useState({Null})
+const{about,setAbout}=useState(null)
     return(
         <ExampleContext.Provider value={{about,setAbout}}>
             {children}
